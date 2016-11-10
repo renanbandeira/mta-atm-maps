aa97643ea0cdde1a596bccd4801cb4a4b278c0e1
Renan Bandeira <email@renanbandeira.com.br>
2016-11-10 15:56:00 -0300

    Add Location Awareness to code

--- app/src/main/java/br/com/renanbandeira/mta_atm_maps/MapsActivity.java
+++ app/src/main/java/br/com/renanbandeira/mta_atm_maps/MapsActivity.java
@@ -1,11 +1,24 @@
 package br.com.renanbandeira.mta_atm_maps;
 
+import android.*;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -15,14 +28,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-    GoogleMap.OnCameraIdleListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraIdleListener,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
   private GoogleMap mMap;
+
+  private LocationRequest mLocationRequest;
+
+  private LocationListener mLocationListener;
 
   private LatLng mPosition;
 
   private Address mAddress;
+
+  private GoogleApiClient mGoogleApiClient;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -30,6 +49,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addConnectionCallbacks(this)
+        .addOnConnectionFailedListener(this)
+        .addApi(LocationServices.API)
+        .build();
+
+    mLocationRequest = new LocationRequest();
+    mLocationRequest.setInterval(10000);
+    mLocationRequest.setSmallestDisplacement(50);
+    mLocationRequest.setFastestInterval(5000);
+    mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    mLocationListener = new LocationListener() {
+      @Override public void onLocationChanged(Location location) {
+        goToLocation(location);
+      }
+    };
   }
 
   /**
@@ -52,7 +87,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     mMap.getUiSettings().setZoomControlsEnabled(true);
     mMap.getUiSettings().setTiltGesturesEnabled(true);
     mMap.setOnCameraIdleListener(this);
+
+    updateMyLocation();
   }
+
+  private boolean hasPermission() {
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) {
+      // TODO: Consider calling
+      ActivityCompat.requestPermissions(this,
+          new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 10);
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+      return false;
+    }
+    return true;
+  }
+
+  @Override public void onRequestPermissionsResult(int requestCode,
+      @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == 10 && grantResults.length > 0 &&
+        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      //Eu tenho a permissão do usuario
+      if (mMap == null) return;
+      updateMyLocation();
+    }
+  }
+
+  private void goToLocation(Location location) {
+    if (location == null) return;
+    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+        new LatLng(location.getLatitude(),
+            location.getLongitude()), 16
+    ));
+  }
+
+  private void updateMyLocation() {
+    if (!hasPermission()) return;
+    mMap.setMyLocationEnabled(true);
+    if (mGoogleApiClient.isConnected()) {
+      Location location = LocationServices.FusedLocationApi
+          .getLastLocation(mGoogleApiClient);
+      goToLocation(location);
+      LocationServices.FusedLocationApi
+          .requestLocationUpdates(mGoogleApiClient,
+              mLocationRequest, mLocationListener);
+    }
+  }
+
   @Override public void onCameraIdle() {
     if(mMap == null) {
       return;
@@ -80,11 +166,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     String snippet = "";
     if (mAddress != null) {
       EditText search = (EditText) findViewById(R.id.search);
-      snippet = mAddress.getFeatureName()
+      snippet = mAddress.getThoroughfare()
           + ", " + mAddress.getSubThoroughfare() + " - "
           + mAddress.getSubLocality()
           + " - "
-          + mAddress.getSubAdminArea()
+          + mAddress.getLocality()
           + ", "
           + mAddress.getAdminArea();
       search.setText(snippet);
@@ -114,5 +200,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
       e.printStackTrace();
       return null;
     }
+  }
+
+  @Override public void onConnected(@Nullable Bundle bundle) {
+    updateMyLocation();
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+    mGoogleApiClient.connect();
+  }
+
+  @Override protected void onStop() {
+    super.onStop();
+    LocationServices.FusedLocationApi
+        .removeLocationUpdates( mGoogleApiClient, mLocationListener);
+    mGoogleApiClient.disconnect();
+  }
+
+  @Override public void onConnectionSuspended(int i) {
+
+  }
+
+  @Override public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
   }
 }
